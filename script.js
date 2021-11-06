ed670cc1296b200e86085579009cc7c05b2f291d
jashanpreetKaur26 <74594701+jashanpreetKaur26@users.noreply.github.com>
2021-11-05 21:55:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+console.log("The number of paragraphs are two");
+let h3e=document.createElement("h3");
+h3e.textContent="Irmanjeet kaur ";
+document.body.insertBefore(h3e,document.body.firstChild)
+
+
+//student name-Irmanjeet kaur
+//student number-
+//date-
